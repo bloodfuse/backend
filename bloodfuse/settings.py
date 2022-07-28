919ec6a5617fcbdc5c85af8e0c8f9c04744700b9
iamgaddiel <ighotagaddiel98@gmail.com>
2022-07-28 07:19:16 -0400

    added bloodfuse.pythonanywhere.com as allowed host to project

--- bloodfuse/settings.py
+++ bloodfuse/settings.py
@@ -18,7 +18,9 @@ SECRET_KEY = ENV.get("APP_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'bloodfuse.pythonanywhere.com',
+]
 
 
 # Application definition
