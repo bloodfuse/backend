2eaa27f2751e60069ab222b0212a3e3e7fa1deaa
iamgaddiel <ighotagaddiel98@gmail.com>
2022-10-05 10:34:59 -0400

    updated cors origin headers

--- bloodfuse/settings.py
+++ bloodfuse/settings.py
@@ -197,17 +197,17 @@ CORS_ALLOW_METHODS = [
     "PUT",
 ]
 
-# CORS_ALLOW_HEADERS = [
-#     "accept",
-#     "accept-encoding",
-#     "authorization",
-#     "content-type",
-#     "dnt",
-#     "origin",
-#     "user-agent",
-#     "x-csrftoken",
-#     "x-requested-with",
-# ]
+CORS_ALLOW_HEADERS = [
+     "accept",
+     "accept-encoding",
+     "authorization",
+     "content-type",
+     "dnt",
+     "origin",
+     "user-agent",
+     "x-csrftoken",
+     "x-requested-with",
+ ]
 
 
 # _________________________________________________________________________________________
