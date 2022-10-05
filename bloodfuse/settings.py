5f8493c0c6098f09ac637055bb664dfe353daf4e
iamgaddiel <ighotagaddiel98@gmail.com>
2022-10-05 11:11:03 -0400

    updated cors origin headers-

--- bloodfuse/settings.py
+++ bloodfuse/settings.py
@@ -209,7 +209,7 @@ CORS_ALLOW_HEADERS = [
      "x-requested-with",
  ]
 
-#CORS_ALLOW_ALL_ORIGIN:  True
+CORS_ALLOW_ALL_ORIGIN:  True
 
 # _________________________________________________________________________________________
 # AUTH
