0698b829af1a98488bbfd64133199b37bbfe8078
scofieldwhitefire <gabrielprecioux@gmail.com>
2022-11-18 15:05:30 -0500

    Added more field

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ from .views import (
     index, 
     DonorListView,
     BloodCentersListView,
+    UserView,
 )
 
 
@@ -15,6 +16,7 @@ router.register("user", UserViewSet)
 
 urlpatterns = [
     path('', index, name="index"),
+    path('user/', UserView.as_view, name="user_view"),
     path('users/donors/', DonorListView.as_view(), name='donor_list'),
     path('users/blood-centers/', BloodCentersListView.as_view(), name='blood_centers_list'),
 ]
