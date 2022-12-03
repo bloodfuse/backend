fdcbf9b95c9d445a70a865f7f78a9e23029be44b
scofieldwhitefire <tetelainv@gmail.com>
2022-12-02 21:53:25 -0300

    Adjustments

--- appointments/serializers.py
+++ appointments/serializers.py
@@ -102,7 +102,7 @@ class RequestBloodSerializer(serializers.ModelSerializer):
 
     def details(user):
         xLists = []
-        user = User.objects.filter(username=str(user))
+        user = User.objects.get(username=str(user))
 
         getRequests = RB.objects.filter(user=user.username)
         if getRequests.exists():
